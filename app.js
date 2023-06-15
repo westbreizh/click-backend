ede5cb48b9f21b56cb469b158a2c234bdd9b5f4a
aurélien <herbreteauaurelien@hotmail.fr>
2023-06-15 16:18:03 +0200

    rtb

--- app.js
+++ app.js
@@ -11,14 +11,8 @@ const cors = require('cors');
 
 const app = express();
 
-// Activer les en-têtes CORS pour toutes les requêtes
-const allowedOrigins = ['http://localhost:3000', 'https://click-and-raquette.com'];
-
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
