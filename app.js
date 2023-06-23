d028470bf58240573d189b20a188de74831dcb9c
aurélien <herbreteauaurelien@hotmail.fr>
2023-06-23 10:55:31 +0200

    coordonnées modifié ou crée

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin','https://click-and-raquette.com','http://localhost:3000/', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
