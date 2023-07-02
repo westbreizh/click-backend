5fd5fff813f699a5bd9e33b2cebb111ee5ce1691
aurélien <herbreteauaurelien@hotmail.fr>
2023-07-02 14:16:51 +0200

    buffer

--- controllers/stripes.js
+++ controllers/stripes.js
@@ -9,7 +9,7 @@ const sendEmail = require("../email/sendEmail")
 // fichier pour se connecter à notre base de donnée
 const db = require("../BDD/database-connect")
 
-
+// fonction d'une session stripe 
 exports.createCheckOutSession = async (req, res) => {
   console.log("je rentre dans stripe backend");
 
@@ -45,7 +45,6 @@ exports.createCheckOutSession = async (req, res) => {
         },
       ],
       mode: 'payment',
-
       success_url: `${YOUR_DOMAIN}/paiement-accepte`,
       cancel_url: `${YOUR_DOMAIN}/paiement-refuse`,
       automatic_tax: { enabled: false },
@@ -93,52 +92,60 @@ function saveInvoiceToDatabase(paymentIntent) {
 
 
 // Endpoint de webhook pour recevoir les événements de Stripe et enclencher les actions appropriées
+
+
+
 exports.actionAfterPaiement = async (req, res) => {
   const sig = req.headers['stripe-signature'];
-  console.log("je rentre dans webhook");
+  console.log("Je rentre dans le webhook");
 
   let event;
+  let body = '';
 
-  try {
-    // Construction de l'événement à partir de la demande et de la signature en utilisant l'endpoint secret
-    event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
-  } catch (err) {
-    // En cas d'erreur lors de la construction de l'événement, renvoyer une réponse d'erreur 400
-    res.status(400).send(`Webhook Error: ${err.message}`);
-    return;
-  }
+  // Lire le corps de la requête en tant que Buffer
+  req.on('data', (chunk) => {
+    body += chunk;
+  });
 
-  // Traitement de l'événement en fonction de son type
-  switch (event.type) {
-    case 'payment_intent.succeeded':
-      console.log(`intention de paiement réalisé : ${event.type}`);
-      saveInvoiceToDatabase(event.data.object);
-      sendEmail(event.data.object.customer_email, 'Confirmation de paiement', {
-        customerName: event.data.object.customer_name,
-        amount: event.data.object.amount / 100, // Conversion du montant en euros
-        paymentDate: new Date(event.data.object.created * 1000).toLocaleDateString('fr-FR'), // Formatage de la date
-        paymentMethod: event.data.object.payment_method_types[0], // Utilisation de la première méthode de paiement
-      }, 
-      './email/template/confirmationPaiementEmail' // Chemin relatif vers le modèle de courrier électronique
-      );
-      break;
-    case 'charge.succeeded':
-      console.log(`charge, paiement réalisé avec succès : ${event.type}`);
+  req.on('end', () => {
+    try {
+      // Construction de l'événement à partir du corps de la requête et de la signature en utilisant l'endpoint secret
+      event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
 
-      // Traiter l'événement de charge réussie
-      break;
-    case 'payment_intent.created':
-      // Traiter l'événement de création d'un nouvel intent de paiement
-      console.log(`intention de paiement créée : ${event.type}`);
-      break;
-    // Ajouter d'autres cas pour les types d'événements supplémentaires que vous souhaitez traiter
-    default:
-      console.log(`Type d'événement non géré : ${event.type}`);
-  }
+      // Traitement de l'événement en fonction de son type
+      switch (event.type) {
+        case 'payment_intent.succeeded':
+          console.log(`Intention de paiement réalisée : ${event.type}`);
+          saveInvoiceToDatabase(event.data.object);
+          sendEmail(event.data.object.customer_email, 'Confirmation de paiement', {
+            customerName: event.data.object.customer_name,
+            amount: event.data.object.amount / 100, // Conversion du montant en euros
+            paymentDate: new Date(event.data.object.created * 1000).toLocaleDateString('fr-FR'), // Formatage de la date
+            paymentMethod: event.data.object.payment_method_types[0], // Utilisation de la première méthode de paiement
+          }, './email/template/confirmationPaiementEmail'); // Chemin relatif vers le modèle de courrier électronique
+          break;
+        case 'charge.succeeded':
+          console.log(`Charge, paiement réalisé avec succès : ${event.type}`);
+          // Traiter l'événement de charge réussie
+          break;
+        case 'payment_intent.created':
+          console.log(`Intention de paiement créée : ${event.type}`);
+          // Traiter l'événement de création d'un nouvel intent de paiement
+          break;
+        // Ajouter d'autres cas pour les types d'événements supplémentaires que vous souhaitez traiter
+        default:
+          console.log(`Type d'événement non géré : ${event.type}`);
+      }
 
-  // Renvoyer une réponse 200 pour accuser réception de l'événement
-  res.send();
+      // Renvoyer une réponse 200 pour accuser réception de l'événement
+      res.send();
+    } catch (err) {
+      res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+  });
 };
+
+
 
 
 
