58f96c8d0bb136543e5308079a0ef92c82a5d798
aurélien <herbreteauaurelien@hotmail.fr>
2023-07-02 09:18:45 +0200

    reponse de stripe après paiement

--- routes/stripes.js
+++ routes/stripes.js
@@ -5,7 +5,7 @@ const stripeCtrl = require('../controllers/stripes');
 
 
 router.post('/create-checkout-session', stripeCtrl.createCheckOutSession);
-
+router.post('/webhook', stripeCtrl.actionAfterPaiement);
 
 
 module.exports = router;
