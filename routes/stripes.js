9fe0af4727d1d878aff5a6aa7c124feba57896f0
aurélien <herbreteauaurelien@hotmail.fr>
2023-07-03 11:49:03 +0200

    rawbody

--- routes/stripes.js
+++ routes/stripes.js
@@ -1,25 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const getRawBody = require('raw-body');
 
-const stripeCtrl = require('../controllers/stripes');
-
+const stripeCtrl = require('../controllers/stripe');
 
 router.post('/create-checkout-session', stripeCtrl.createCheckOutSession);
-router.post('/webhook', async (req, res, next) => {
-    console.log("dans route"+req)
-
-    try {
-
-      req.rawBody = await getRawBody(req);
-      console.log("dans route"+req.rawBody)
-      next();
-    } catch (err) {
-      next(err);
-    }
-  }, stripeCtrl.actionAfterPaiement);
-  
-
+router.post('/webhook', express.raw({ type: 'application/json' }), stripeCtrl.actionAfterPaiement);
 
 module.exports = router;
-
