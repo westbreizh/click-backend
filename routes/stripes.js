183e0d737528af068318f339de3927e4fe5fd163
aurélien <herbreteauaurelien@hotmail.fr>
2023-07-03 20:57:01 +0200

    refonte

--- routes/stripes.js
+++ routes/stripes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const stripeCtrl = require('../controllers/stripes');
+const bodyParser = require('body-parser');
 
+router.use(bodyParser.raw({ type: 'application/json' }));
 
 router.post('/create-checkout-session', stripeCtrl.createCheckOutSession);
 router.post('/webhook', express.raw({type: 'application/json', limit: '10mb'}), stripeCtrl.actionAfterPaiement);
