cc2065ed0003eba0da603fc032dfa9dfe5a28fa3
aurélien <herbreteauaurelien@hotmail.fr>
2023-07-03 12:24:32 +0200

    rawbody

--- routes/stripes.js
+++ routes/stripes.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 const stripeCtrl = require('../controllers/stripes');
 
+router.use(bodyParser.raw({ type: 'application/json' }));
 
 router.post('/create-checkout-session', stripeCtrl.createCheckOutSession);
-router.post('/webhook', express.raw({ type: 'application/json' }), stripeCtrl.actionAfterPaiement);
-
-  
-
+router.post('/webhook', stripeCtrl.actionAfterPaiement);
 
 module.exports = router;
-
