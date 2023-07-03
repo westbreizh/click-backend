cf2c374874cf79b6a96ca769e877bcb88a1696c0
aurélien <herbreteauaurelien@hotmail.fr>
2023-07-03 09:13:44 +0200

    nobuffer

--- server.js
+++ server.js
@@ -21,7 +21,15 @@ const normalizePort = val => {
 };
 
 
+const cors = require('cors');
+app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 
 
