d5a16bd10ccf42b7ab89cfa104cbae2b2978b9a9
aurélien <herbreteauaurelien@hotmail.fr>
2023-07-10 17:53:21 +0200

    page order

--- controllers/user.js
+++ controllers/user.js
@@ -399,7 +399,7 @@ exports.sendOrderLog = (req, res, next) => {
 
 exports.sendOneOrder = (req, res, next) => {
   console.log("req.body", req.body);
-  const orderId = req.body.orderId
+  const orderId = req.body.orderId.orderId
   console.log("req.body.orderId", req.body.orderId);
     db.query(`SELECT * FROM orders WHERE id='${orderId}'`, (err, result) => {
       if (err) {
