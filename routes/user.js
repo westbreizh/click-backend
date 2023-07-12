9d8d0f1d3dfb089a8ad4bb9996e0a0dd0f379b10
aurélien <herbreteauaurelien@hotmail.fr>
2023-07-12 19:22:56 +0200

    token

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authenticateJWT = require('../middleware/authenticateJWT'); 
+const authenticateJWT = require('../middleware/authenticateJWT '); 
 const userCtrl = require('../controllers/user');
 
 
