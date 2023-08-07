1e3584af1af9c17a08f80ff0a531e21f3463cf0f
aurélien <herbreteauaurelien@hotmail.fr>
2023-08-07 10:28:26 +0200

    navbaradmin

--- routes/shop.js
+++ routes/shop.js
@@ -9,6 +9,7 @@ router.post('/productListRandom', shopCtrl.productListRandom);
 router.post('/stringListFiltered', shopCtrl.stringListFiltered);
 router.post('/ballListFiltered', shopCtrl.ballListFiltered);
 router.post('/accessoriesListFiltered', shopCtrl.accessoriesListFiltered);
+router.post('/productSelected', shopCtrl.productSelected);
 router.post('/listHubCollect', shopCtrl.listHubCollect);
 router.post('/listHubWithdrawal', shopCtrl.listHubWithdrawal);
 
