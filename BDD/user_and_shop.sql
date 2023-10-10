2af41f938bd543ed77afaba00e95374eefbeac52
aurélien <herbreteauaurelien@hotmail.fr>
2023-10-10 15:22:20 +0200

    erbt

--- BDD/user_and_shop.sql
+++ BDD/user_and_shop.sql
@@ -230,19 +230,21 @@ logoMark_url                VARCHAR(255),
 description_text            VARCHAR(255)
 );
 
-INSERT INTO string (categorie, mark, model, composition, packaging, price, first_characteristic, control_rating, power_rating, spin, comfort, durability_rating, gauge, color, image_url, logoMark_url, description_text)
+INSERT INTO `string`(categorie, mark, model, composition, packaging, price, first_characteristic, control_rating, power_rating, spin, comfort, durability_rating, gauge, color, image_url, logoMark_url, description_text)
 VALUES 
+
+('cordage', 'Tecnifibre', 'TGV', 'Multifilaments', 'garniture 12m', 32, 'Confort', 8, 5, 4, 9, 5, '1.25 mm', 'blanc', 'https://click-backend.herokuapp.com/technifibre-tgv.jpg', 'https://click-backend.herokuapp.com/logo-tecnifibre.webp',
+ 'Le cordage Tecnifibre TGV est un multifilament axé sur le confort et le contrôle, idéal pour les joueurs souffrant de tennis elbow, bien qu\'il ne soit pas très puissant. Il se distingue par sa stabilité de tension.'),
+
+('cordage', 'Tecnifibre', 'Black Code', 'Monofilament', 'garniture 12m', 28, 'Prise d\'effet', 8, 5, 8, 4, 7, '1.32 mm', 'noire', 'https://click-backend.herokuapp.com/technifibre-black-code.jpg', 'https://click-backend.herokuapp.com/logo-tecnifibre.webp',
+ 'Le cordage Tecnifibre Black Code est un cordage en co-polyester pentagonal qui intègre la technologie Thermacore de Tecnifibre. Cette innovation apporte un confort exceptionnel pour un cordage monofilament, offrant à la fois souplesse, confort, et vitesse de balle. Sa forme pentagonale optimise également la prise d\'effet.'),
+
 ('cordage', 'Technifibre', 'X-One Biphase', 'Multifilament', 'garniture 12m', 37, 'Confort', 7, 8, 6, 9, 5, '1.24 mm', 'noir', ' https://click-backend.herokuapp.com/technifibre-biphase-12m.webp', 'https://click-backend.herokuapp.com/logo-tecnifibre.webp', 
 'Le cordage multifilament Xone Biphase est considéré comme le meilleur multifilament du marché par de nombreux joueurs de tennis. Il offre une combinaison exceptionnelle de durée de vie, de confort, de contrôle, d\'effets et de puissance.'),
 
 ('cordage', 'Tecnifibre', 'Duramix', 'Multifilaments', 'garniture 12m', 28, 'Contrôle', 8, 6, 6, 7, 4, '1.25 mm', 'blanc', 'https://click-backend.herokuapp.com/technifibre-duramix.jpg', 'https://click-backend.herokuapp.com/logo-tecnifibre.webp',
- 'Le Duramix est conçu pour offrir le meilleur compromis entre confort et contrôle. Malgré sa puissance modérée, il excelle par sa polyvalence.'),
+ 'Le Duramix est conçu pour offrir le meilleur compromis entre confort et contrôle. Malgré sa puissance modérée, il excelle par sa polyvalence.');
 
-('cordage', 'Tecnifibre', 'TGV', 'Multifilaments', 'garniture 12m', 32, 'Confort', 8, 5, 4, 9, 5, '1.25 mm', 'blanc', 'https://click-backend.herokuapp.com/technifibre-tgv.jpg', 'https://click-backend.herokuapp.com/logo-tecnifibre.webp',
- 'Le cordage Tecnifibre TGV est un multifilament axé sur le confort et le contrôle, idéal pour les joueurs souffrant de tennis elbow, bien qu'il ne soit pas très puissant. Il se distingue par sa stabilité de tension.'),
-
-('cordage', 'Tecnifibre', 'TGV', 'Monofilament', 'garniture 12m', 28, 'Prise d\'effet', 8, 5, 8, 4, 7, '1.32 mm', 'noire', 'https://click-backend.herokuapp.com/technifibre-black-code.jpg', 'https://click-backend.herokuapp.com/logo-tecnifibre.webp',
- 'Le cordage Tecnifibre Black Code est un cordage en co-polyester pentagonal qui intègre la technologie Thermacore de Tecnifibre. Cette innovation apporte un confort exceptionnel pour un cordage monofilament, offrant à la fois souplesse, confort, et vitesse de balle. Sa forme pentagonale optimise également la prise d\'effet.');
 
 
 
