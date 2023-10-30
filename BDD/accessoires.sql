f2665e3b9b78f540720665cce00caa9f7c9af776
westbreizh <herbreteauaurelien@hotmail.fr>
2023-10-30 11:35:10 +0100

    bdd modifié

--- BDD/accessoires.sql
+++ BDD/accessoires.sql
@@ -14,14 +14,9 @@ description_text    VARCHAR(255)
 
 
 INSERT INTO accessories (categorie, mark, model, price, type_product, logoMark_url, image_url, description_text) VALUES
-('accessoire', 'Babolat',' My Overgrip 3 unités', 8.50, 'surgrip', '    http://localhost:3001/logo-babolat.webp', '    http://localhost:3001/3-surgrip-babolat.webp',
- 'Le grip Babolat my overgrip est conçu pour offrir une excellente adhérence et une absorption optimale de la transpiration.'),
 
-('accessoire', 'Tourna', 'Mega Tac Blanc  ', 9.90, 'surgrip','     http://localhost:3001/logo-tourna.webp', '    http://localhost:3001/10-tourna-surgrip.webp',
-'Les Surgrips Mega Tac possèdent un effet collant et sont très résistants. '),
-
-('accessoire', 'Tourna', 'Tuff 3 unités', 9.50, 'surgrip','     http://localhost:3001/logo-tourna.webp', '    http://localhost:3001/3-tourna-surgrip.webp',
-'Le surgrip Tourna Tuff est conçu pour offrir un excellent confort et une absorption optimale de la transpiration.');
+('accessoire', 'Wilson', 'Pro overgrip', 7, 'surgrip','http://localhost:3001/Wilson-logo.webp', '    http://localhost:3001/surgrips-wilson-pro-overgrip-blanc-x-3.jpg',
+'Le Pro overgrip est conçu pour offrir un excellent confort et une absorption optimale de la transpiration.');
 
 
 
