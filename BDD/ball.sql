5abdea4e66266e4a7aab992c886ba7848359bb6a
westbreizh <herbreteauaurelien@hotmail.fr>
2023-10-30 11:21:27 +0100

    bdd modifié

--- BDD/ball.sql
+++ BDD/ball.sql
@@ -21,9 +21,8 @@ VALUES
 ('balle', 'Tecnifibre', 'X-one', 8.5, 'tube de 4 balles', 'standard', 'https://click-backend.herokuapp.com/logo-tecnifibre.webp', 'https://click-backend.herokuapp.com/balle-xone.jpg' ,
 'La X-One est la balle haut de gamme de Tecnifibre. La X-One est une balle vive, ultra performante et qui possède une excellente longévité.'),
 
-
-('balle', 'Tecnifibre', 'Club', 7.5, 'tube de 4 balles', 'standard', 'https://click-backend.herokuapp.com/logo-tecnifibre.webp', 'https://click-backend.herokuapp.com/tube-de-4-balles-tecnifibre-club.jpg' ,
-'Un peu moins résistante et qualitative que la Xone, cette balle garde néanmoins de très bonne qualités');
+('balle', 'Dunlop', 'Championship', 7, 'tube de 4 balles', 'standard', 'https://click-backend.herokuapp.com/logo-tecnifibre.webp', 'https://click-backend.herokuapp.com/tube-de-4-balles-dunlop-atp-championship.jpg' ,
+'Un peu moins résistante et qualitative que la Xone de tecnifibre, cette balle garde néanmoins de très bonne qualités et un rapport qualité prix imbattable');
 
 
 
