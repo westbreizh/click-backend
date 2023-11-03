cd546cb569ac7d4f72ec1bac678588b40163299f
westbreizh <herbreteauaurelien@hotmail.fr>
2023-11-03 09:25:28 +0100

    session verification

--- middleware/sessionMiddleware.js
+++ middleware/sessionMiddleware.js
@@ -9,7 +9,47 @@ const sessionMiddleware = (req, res, next) => {
       secure: true, // Utilisez HTTPS pour sécuriser la communication
     });
   }
+  console.log("Nouvel identifiant de session généré :", sessionId);
   next();
 };
 
 module.exports = sessionMiddleware;
+
+
+
+
+
+/*const { v4: uuidv4 } = require('uuid');
+
+// Middleware de génération et de vérification de l'identifiant de session
+const sessionMiddleware = (req, res, next) => {
+  const sessionId = req.cookies.sessionId;
+
+  // Si l'identifiant de session n'existe pas ou n'est pas valide, générer un nouvel identifiant
+  if (!sessionId || !isValidSession(sessionId)) {
+    const newSessionId = uuidv4();
+    res.cookie('sessionId', newSessionId, {
+      httpOnly: true,
+      secure: true,
+    });
+  }
+
+  next();
+};
+
+// Fonction pour vérifier la validité de l'identifiant de session
+function isValidSession(sessionId) {
+  // Implémentez votre logique de vérification ici
+  // Vous pouvez vérifier si l'identifiant de session existe dans une base de données,
+  // s'il n'a pas expiré, s'il est associé à un utilisateur authentifié, etc.
+  // Si l'identifiant de session est invalide, renvoyez false ; sinon, renvoyez true.
+  // Vous pouvez également gérer le blocage de l'accès aux ressources protégées ici.
+
+  // Exemple de vérification de validité simple : retourne true si l'identifiant est non vide.
+  return sessionId && sessionId.trim() !== '';
+}
+
+module.exports = sessionMiddleware;
+
+
+*/
