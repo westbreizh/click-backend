f5950fa3225e96ffe6ca857159f230fd41a28ff0
westbreizh <herbreteauaurelien@hotmail.fr>
2023-11-03 19:22:05 +0100

    modif de la gestion du token

--- middleware/authenticateJWT .js
+++ middleware/authenticateJWT .js
@@ -5,23 +5,29 @@ const Token_Secret_Key = process.env.TOKEN_SECRET_KEY;
 const jwt = require('jsonwebtoken');
 
 
-// Middleware d'authentification et d'autorisation basé sur les JWT
+const cookie = require('cookie');
+
+// Middleware d'authentification et d'autorisation basé sur les JWT avec récupération du token depuis le cookie
 const authenticateJWT = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1]; // On extrait le token du header "Authorization" en prenant la partie située après l'espace
+  // Analyser les cookies de la requête
+  const cookies = cookie.parse(req.headers.cookie || '');
+
+  // Récupérer le token du cookie 'mon_token'
+  const token = cookies.mon_token;
 
   if (!token) {
     return res.status(401).json({ error: 'Token manquant' });
   }
 
   try {
-    const decodedToken = jwt.verify(token, Token_Secret_Key ); // On décode le token en utilisant la clé secrète, c'est cette fonction qui verifie la validité du token reçu
+    const decodedToken = jwt.verify(token, Token_Secret_Key);
 
     // Vous pouvez ajouter des vérifications supplémentaires ici selon vos besoins
     // Par exemple, vérifier si l'utilisateur existe dans la base de données, ses autorisations, etc.
 
     // Passer les informations du token décodé à la demande pour une utilisation ultérieure
     req.user = decodedToken;
-    console.log("l'auhentification via le token réussi")
+    console.log("l'authentification via le token réussie")
     next(); // Si l'authentification et l'autorisation sont réussies, on passe à l'étape suivante
   } catch (error) {
     return res.status(401).json({ error: 'Token invalide' });
