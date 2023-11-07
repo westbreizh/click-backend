e0b979b1b6cae7468dba8cc0dcc30aaaca477c9f
westbreizh <herbreteauaurelien@hotmail.fr>
2023-11-07 11:33:43 +0100

    rate limit

--- middleware/rate-limit.js
+++ middleware/rate-limit.js
@@ -1,11 +1,11 @@
-const rateLimit = require('express-rate-limit'); // package pour prévenir des attaques par force brute
-
+const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 5, // limite chaque IP à 5 requêtes par fenêtre
-  message: "Trop de tentatives de connexion, veuillez réessayer dans un quart d'heure."
+  handler: function(req, res, /*next*/) {
+    res.status(429).json({message: "Trop de tentatives de connexion, veuillez réessayer dans un quart d'heure."});
+  }
 });
 
-      
- module.exports = limiter;
+module.exports = limiter;
