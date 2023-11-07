c51b730f7eb2141a01c40a8d277ab0d2a1b50a89
westbreizh <herbreteauaurelien@hotmail.fr>
2023-11-07 15:04:36 +0100

    valdationInput

--- middleware/validationInput/validateLogin.js
+++ middleware/validationInput/validateLogin.js
@@ -2,14 +2,14 @@ const Joi = require('joi');
 
 const schema = Joi.object({
   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'fr'] } }).required()
+  email: Joi.string().email({ minDomainSegments: 2 }).required()
 });
 
 function validateLogin(req, res, next) {
     console.log('req.body', req.body);
   const { error } = schema.validate(req.body);
   if (error) {
-    res.status(400).send(error.details[0].message);
+    res.status(400).json({message: "Vos données ne sont pas valides."});
   } else {
     next();
   }
