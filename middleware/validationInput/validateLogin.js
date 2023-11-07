58d3409dc4cbdce77e7234aeb61c245db1191c56
westbreizh <herbreteauaurelien@hotmail.fr>
2023-11-07 15:07:12 +0100

    valdationInput

--- middleware/validationInput/validateLogin.js
+++ middleware/validationInput/validateLogin.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const schema = Joi.object({
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    password: Joi.string(),
   email: Joi.string().email({ minDomainSegments: 2 }).required()
 });
 
