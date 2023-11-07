49d8c74ea0690c11c95492f3cb0ae4298085b0a8
westbreizh <herbreteauaurelien@hotmail.fr>
2023-11-07 15:02:16 +0100

    valdationInput

--- middleware/validationInput/validateLogin.js
+++ middleware/validationInput/validateLogin.js
@@ -1,10 +1,30 @@
 const Joi = require('joi');
 
 const schema = Joi.object({
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'fr'] } }).required()
+});
+
+function validateLogin(req, res, next) {
+    console.log('req.body', req.body);
+  const { error } = schema.validate(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+  } else {
+    next();
+  }
+}
+
+module.exports = validateLogin;
+
+/*
+const Joi = require('joi');
+
+const schema = Joi.object({
     password: Joi.string()
     .pattern(new RegExp('^(?=.{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$'))
     .required(),
-  
+    email: Joi.string().email({ minDomainSegments: 2 }).required()
 });
 
 function validateLogin(req, res, next) {
@@ -18,3 +38,4 @@ function validateLogin(req, res, next) {
 }
 
 module.exports = validateLogin;
+*/
