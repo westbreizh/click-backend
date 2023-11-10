16ae61b052eda25a27ebf2c512f3352a9e072e1b
westbreizh <herbreteauaurelien@hotmail.fr>
2023-11-10 15:06:58 +0100

    proct list modifié

--- routes/shop.js
+++ routes/shop.js
@@ -5,9 +5,6 @@ const authenticateJWTandXSRF = require('../middleware/authenticateJWTandXSRF ');
 const validatePaiementInShop = require('../middleware/validationInput/validatePaiementInShop');
 
 router.post('/productListRandom', shopCtrl.productListRandom);
-router.post('/stringListFiltered', shopCtrl.stringListFiltered);
-router.post('/ballListFiltered', shopCtrl.ballListFiltered);
-router.post('/accessoriesListFiltered', shopCtrl.accessoriesListFiltered);
 router.post('/productSelected', shopCtrl.productSelected);
 router.post('/listHubCollect', shopCtrl.listHubCollect);
 router.post('/listHubWithdrawal', shopCtrl.listHubWithdrawal);
